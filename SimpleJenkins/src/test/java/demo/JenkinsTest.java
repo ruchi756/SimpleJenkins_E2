eac7df08ea67952d56ff3ca4f17392670a60a4cc
ruchi <ruchi@INBOOK_Y2_PLUS>
2025-02-06 13:10:06 +0530

    first commit

---
+++ SimpleJenkins/src/test/java/demo/JenkinsTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void sample() {
+		Reporter.log("JenkinsTest class executed",true);
+		
+	}
+
+}
